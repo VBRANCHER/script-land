7bfabd6e9a60ca9c6b7749472ed16ac0cd2c9bc9
VBRANCHER <42853130+VBRANCHER@users.noreply.github.com>
2018-09-05 21:47:24 -0300

    Add files via upload

--- js/teste.js
+++ js/teste.js
@@ -3,11 +3,13 @@ class teste extends Phaser.Scene {
 	{
 		super({key: 'Teste'});
 		var gameOver = false;
+		var tempo;
+		var timedEvent;
+		var total = 0; 
 	}
 	
 	preload ()
 	{
-		
 		this.load.image('pergunta', 'assets/pergunta.png');
 		this.load.image('buttonA', 'assets/buttonA.png');
 		this.load.image('buttonB', 'assets/buttonB.png');
@@ -19,9 +21,12 @@ class teste extends Phaser.Scene {
 
 	create ()
 	{
+		this.timedEvent = this.time.delayedCall(5000, this.gameOver, this.updateCounter, this);
 
 		this.add.image(640, 360, 'pergunta');
 		
+		this.tempo = this.add.text(500,76);
+
 		this.add.image(180, 500, 'Protagonista1');
 		
 		this.add.image(1100, 500, 'Vilão1');
@@ -31,8 +36,21 @@ class teste extends Phaser.Scene {
 		document.getElementById("divPergunta").style.visibility = "visible";
 		
 	}
+
+	updateCounter() 
+	{
+		total++;
+		
+
+		
+	}
 	update ()
 	{	
+		this.tempo.setText(' ' + this.timedEvent.getProgress().toString().substr(0,1));
 		
+	}
+
+	gameOver(){
+		game.scene.switch('Teste','game_over');
 	}
 }
