0144ead9b784b4b0dc9687d7594cb0875db23eb6
E-Arky <42853121+E-Arky@users.noreply.github.com>
2018-09-06 19:51:35 -0300

    Add files via upload

--- js/selecionaPersonagem.js
+++ js/selecionaPersonagem.js
@@ -10,6 +10,8 @@ class selecionaPersonagem extends Phaser.Scene {
 		
 		this.load.image('selecionaPersonagem', 'assets/selecionaPersonagem.png');
 		this.load.image('seta_avancar', 'assets/seta_avancar.png');
+		this.load.image('p1', 'assets/selecionar1.png');
+		this.load.image('p2', 'assets/selecionar2.png');
 		
 	}
 
@@ -17,8 +19,14 @@ class selecionaPersonagem extends Phaser.Scene {
 	{
 
 		this.add.image(640, 360, 'selecionaPersonagem');
-		
+		/*
 		this.startBtn = this.add.sprite(610, 650, 'seta_avancar').setInteractive();
+		this.startBtn.on('pointerdown', function (event) {game.scene.switch('selecionaPersonagem','tutorial'); });  */
+		
+		this.startBtn = this.add.sprite(178, 390, 'p1').setInteractive();
+		this.startBtn.on('pointerdown', function (event) {game.scene.switch('selecionaPersonagem','selecionaPersonagem'); });
+		
+		this.startBtn = this.add.sprite(482, 390, 'p2').setInteractive();
 		this.startBtn.on('pointerdown', function (event) {game.scene.switch('selecionaPersonagem','tutorial'); });
 		
 	}
