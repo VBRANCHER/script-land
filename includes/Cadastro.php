77b6b270c17c5ca99cf62a6fb58e15f0ffe9d9ce
E-Arky <42853121+E-Arky@users.noreply.github.com>
2018-09-10 19:56:39 -0300

    Add files via upload

--- includes/Cadastro.php
+++ includes/Cadastro.php
@@ -7,16 +7,19 @@ include_once("banco_de_dados.php");
 
 
 if (isset($_POST["email"])){
+	
 	$array = select("SELECT * FROM jogadores WHERE email='{$_POST["email"]}' AND senha='{$_POST["senha"]}'");
 	
-	if ($array == 0){
+	if ($array != 0){
 	
-	echo "<script>alert ('Usuário e/ou senha incorretos');</script>";
+		echo "<script>alert ('Erro ao cadastrar, o e-mail já está cadastrado');</script>";
 
 	}
 	else{
-	
-	$_SESSION["arrUsuario"] = $array[0];
+		
+		$insert = "INSERT INTO jogadores VALUES (DEFAULT, '{$_POST["name"]}', 'teste', '{$_POST["email"]}', '{$_POST["senha"]}', '2018-09-10')";
+		
+		$_SESSION["arrUsuario"] = php_insert($insert);
 	
 	}
 }
