92deb28a59dbbdfa41f938f30c73a62b129eb5ca
ZeBanana <42851436+ZeBanana@users.noreply.github.com>
2018-09-13 22:01:38 -0300

    Add files via upload

--- index11.php
+++ index11.php
@@ -88,6 +88,10 @@
 
 <script type="text/javascript">
 
+var testeObj = new teste();
+var testeObj2 = new teste2();
+var testeObj3 = new teste3();
+
 var config = {
     type: Phaser.AUTO,
     width: 1280,
@@ -101,7 +105,7 @@ var config = {
         }
     },
 	
-	scene: [selecionaPersonagem, tutorial, loja, spawn, teste, teste2, teste3, loja_dentro, game_over, salvar, lugar_legal]
+	scene: [selecionaPersonagem, tutorial, loja, spawn, testeObj, testeObj2, testeObj3, loja_dentro, game_over, salvar, lugar_legal]
 };
 var game = new Phaser.Game(config);
 game.scene.start(selecionaPersonagem);
