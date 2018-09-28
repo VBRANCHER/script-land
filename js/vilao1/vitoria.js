37465de6a118aab5bebfe4026df573df3c34b830
VBRANCHER <42853130+VBRANCHER@users.noreply.github.com>
2018-09-27 21:52:51 -0300

    Add files via upload

---
+++ js/vilao1/vitoria.js
@@ -0,0 +1,29 @@
+class vitoria extends Phaser.Scene {
+	constructor ()
+	{
+		super({key: 'vitoria'});
+		var gameOver = false;
+	}
+	
+	preload ()
+	{
+		
+		this.load.image('vitoria', 'assets/vitoria.png');
+		this.load.image('voltar', 'assets/setaVoltar.png');
+		
+	}
+
+	create ()
+	{
+
+		this.add.image(640, 360, 'vitoria');
+		
+		this.startBtn = this.add.sprite(240, 650, 'voltar').setInteractive();
+		this.startBtn.on('pointerdown', function (event) {game.scene.switch('vitoria','loja'); });
+		
+	}
+	update ()
+	{	
+		
+	}
+}
