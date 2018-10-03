46256b78239bd4ad57a851b2c6dc0b5f72704656
E-Arky <42853121+E-Arky@users.noreply.github.com>
2018-10-03 19:14:57 -0300

    Add files via upload

--- js/game_over.js
+++ js/game_over.js
@@ -9,7 +9,6 @@ class game_over extends Phaser.Scene {
 	{
 		
 		this.load.image('game_over', 'assets/game_over.png');
-		this.load.image('sair', 'assets/sair.png');
 		this.load.image('tentar_novamente', 'assets/tentar_novamente.png');
 	}
 
@@ -20,9 +19,6 @@ class game_over extends Phaser.Scene {
 		
 		this.startBtn = this.add.sprite(510, 396, 'tentar_novamente').setInteractive();
 		this.startBtn.on('pointerdown', function (event) {game.scene.switch('game_over','loja'); });
-		
-		this.startBtn = this.add.sprite(790, 396, 'sair').setInteractive();
-		this.startBtn.on('pointerdown', function (event) {game.scene.switch('game_over','selecionaPersonagem'); });
 		
 	}
 	update ()
