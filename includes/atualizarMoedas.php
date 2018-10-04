d44552114503073ad0e1f9e33ff75224481d0441
E-Arky <42853121+E-Arky@users.noreply.github.com>
2018-10-03 21:30:32 -0300

    Add files via upload

--- includes/atualizarMoedas.php
+++ includes/atualizarMoedas.php
@@ -5,9 +5,10 @@ include_once("banco_de_dados.php");
 if(isset($_GET["moeda"]))
 {
 	sql_delete_update("UPDATE perfis SET moedas = '{$_GET["moeda"]}' WHERE jogadores_id={$_SESSION["arrUsuario"]["id"]}");
-	sql_delete_update("UPDATE perfis SET moedas = '{$_GET["vidas"]}' WHERE jogadores_id={$_SESSION["arrUsuario"]["id"]}");
-	
 }
 
+if(isset($_GET["vida"])){
+	sql_delete_update("UPDATE perfis SET vida = '{$_GET["vida"]}' WHERE jogadores_id={$_SESSION["arrUsuario"]["id"]}");
+}
 
 ?>
