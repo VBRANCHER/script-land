3ea4a8b4f6e8fd42b5af452d6f575374a766d92a
VBRANCHER <42853130+VBRANCHER@users.noreply.github.com>
2018-10-08 22:05:33 -0300

    Add files via upload

---
+++ js/vilao3/teste8.js
@@ -0,0 +1,71 @@
+class teste8 extends Phaser.Scene {
+	constructor ()
+	{
+		super({key: 'teste8'});
+		var gameOver = false;		
+	}
+	
+	preload ()
+	{
+		this.load.image('pergunta', 'assets/pergunta.png');
+		this.load.image('problema8', 'assets/vilao3/problema8.png');
+		this.load.image('buttonA8', 'assets/vilao3/buttonA8.png');
+		this.load.image('buttonB8', 'assets/vilao3/buttonB8.png');
+		this.load.image('buttonC8', 'assets/vilao3/buttonC8.png');
+		this.load.image('buttonD8', 'assets/vilao3/buttonD8.png');
+		this.load.image('Protagonista1', 'assets/Protagonista1.png');
+		this.load.image('Protagonista2', 'assets/Protagonista2.png');
+		this.load.image('Vilão1', 'assets/Vilão1.png');
+	}
+
+	create ()
+	{
+		
+		this.add.image(640, 360, 'pergunta');
+
+		this.add.image(650, 510, 'problema8');
+	
+		if (personagem == 1){
+			this.add.image(180, 500, 'Protagonista1');
+		}
+		if (personagem == 2){
+			this.add.image(180, 500, 'Protagonista2');
+		}
+		
+		this.add.image(1100, 500, 'Vilão1');
+		
+		this.vidasText = this.add.text(10, 48, 'Vidas: ' + vidasRestantes, { font: '32px Arial', fill: '#000000' });
+		
+		this.startBtn = this.add.sprite(506, 596, 'buttonA8').setInteractive();
+		this.startBtn.on('pointerdown', function (event) {testeObj.updateVidas();});
+
+		this.startBtn = this.add.sprite(506, 674, 'buttonB8').setInteractive();
+		this.startBtn.on('pointerdown', function (event) {game.scene.switch('teste8','vitoria3');});
+
+		this.startBtn = this.add.sprite(785, 596, 'buttonC8').setInteractive();
+		this.startBtn.on('pointerdown', function (event) {testeObj.updateVidas();});
+
+		this.startBtn = this.add.sprite(790, 674, 'buttonD8').setInteractive();
+		this.startBtn.on('pointerdown', function (event) { testeObj.updateVidas();});
+		
+	}
+	
+	
+	
+	updateVidas ()
+    {	
+		vidasRestantes--; 
+        testeObj.vidasText.setText('Vidas: ' + vidasRestantes);
+		
+		if (vidasRestantes < 0){
+			vidasRestantes = 3;
+			testeObj8.vidasText.setText('Vidas: ' + vidasRestantes);
+			game.scene.switch('Teste','game_over');
+		}
+    }
+	
+	update ()
+	{	
+		
+	}
+}
---
+++ js/vilao3/vitoria3.js
@@ -0,0 +1,29 @@
+class vitoria3 extends Phaser.Scene {
+	constructor ()
+	{
+		super({key: 'vitoria3'});
+		var gameOver = false;
+	}
+	
+	preload ()
+	{
+		
+		this.load.image('congratulation2', 'assets/congratulation2.png');
+		this.load.image('voltar', 'assets/setaVoltar.png');
+		
+	}
+
+	create ()
+	{
+
+		this.add.image(640, 360, 'congratulation2');
+		
+		this.startBtn = this.add.sprite(240, 650, 'voltar').setInteractive();
+		this.startBtn.on('pointerdown', function (event) {game.scene.switch('vitoria3','lago_legal'); });
+		
+	}
+	update ()
+	{	
+		
+	}
+}
