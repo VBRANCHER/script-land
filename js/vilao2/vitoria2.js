ba44bb9122506cb82af11452a3173f898ff495a0
ZeBanana <42851436+ZeBanana@users.noreply.github.com>
2018-10-11 20:40:42 -0300

    Add files via upload

--- js/vilao2/vitoria2.js
+++ js/vilao2/vitoria2.js
@@ -8,7 +8,7 @@ class vitoria2 extends Phaser.Scene {
 	preload ()
 	{
 		
-		this.load.image('vitoria', 'assets/vitoria.png');
+		this.load.image('vitoria2', 'assets/vitoria2.png');
 		this.load.image('voltar', 'assets/setaVoltar.png');
 		
 	}
@@ -16,7 +16,7 @@ class vitoria2 extends Phaser.Scene {
 	create ()
 	{
 
-		this.add.image(640, 360, 'vitoria');
+		this.add.image(640, 360, 'vitoria2');
 		
 		this.startBtn = this.add.sprite(240, 650, 'voltar').setInteractive();
 		this.startBtn.on('pointerdown', function (event) {game.scene.switch('vitoria2','spawn'); });
